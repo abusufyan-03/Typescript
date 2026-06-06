51601bef0f65127b139702fc91e5fd9be7e5540e
sufiyan <sufiyan.manuu@gmail.com>
2026-06-06 15:25:09 +0530

    installed react + typescript

---
+++ reactTs/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        
+        <div>
+          <h1>Get started</h1>
+        </div>
+        
+      </section>
+    </>
+  )
+}
+
+export default App
